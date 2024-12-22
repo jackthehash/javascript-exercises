6979c342d62b1658b36533541187c938ecc9a862
jackthehash <ltwltw552@gmail.com>
2024-12-22 22:07:04 +0800

    finished 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let reverse = "";
+    let length = str.length;
+    for (let i = 0;i < length;i++) {
+        reverse += str.charAt(length - 1 - i);
+    }
+    return reverse;
 };
 
 // Do not edit below this line
