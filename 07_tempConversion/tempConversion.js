c1eb6660720ae6c3a059d083c5861d50563ad945
jackthehash <ltwltw552@gmail.com>
2024-12-22 23:11:21 +0800

    finished 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  let num = 0;
+  num = (temp - 32) * 5 / 9;
+  num = Math.round(num * 10) / 10
+  return num;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  let num = 0;
+  num = temp * 9 / 5 + 32;
+  num = Math.round(num * 10) / 10
+  return num;
 };
 
 // Do not edit below this line
