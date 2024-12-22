f4beb4e88b95354ee4088b950669072eb6e67dbd
jackthehash <ltwltw552@gmail.com>
2024-12-22 22:58:44 +0800

    finished 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+    if (a < 0 || b < 0) return "ERROR";
+    if (!Number.isInteger(a) || !Number.isInteger(b)) return "ERROR";
+    if (typeof(a) !== "number" || typeof(b) !== "number") return "ERROR";
+    let sum = 0;
+    let i = a;
+    let j = b;
+    if (j < i) {
+        i = b;
+        j = a;
+    }
+    
+    while (i <= j) {
+        sum += i;
+        i++;
+    }
+    return sum;
 };
 
 // Do not edit below this line
