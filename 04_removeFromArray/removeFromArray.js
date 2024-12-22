e14fed7e48b768cdd0cb67d65a1a9214b6e46de2
jackthehash <ltwltw552@gmail.com>
2024-12-22 22:44:02 +0800

    finished 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+    for (let i = 0;i < array.length;i++) {
+        for (const item of args) {
+            if (array[i] === item) {
+                array.splice(i, 1);
+                i--;
+            }
+        }
+    }
+    return array;
 };
-
+    
 // Do not edit below this line
 module.exports = removeFromArray;
