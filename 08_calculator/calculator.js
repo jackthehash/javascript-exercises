912ddab7b033d6264843704b22b5efef48dbdc3e
jackthehash <ltwltw552@gmail.com>
2025-01-01 22:21:28 +0800

    finished 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,39 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  let ans = 0;
+	for (let item of arr) {
+    ans += item;
+  }
+  return ans;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  let ans = 1;
+	for (let item of arr) {
+    ans *= item;
+  }
+  return ans;
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	let ans = 1;
+	for (let i = 0; i < b; i++) {
+    ans *= a;
+  }
+  return ans;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	if (a === 0 || a === 1) {
+    return 1;
+  } return a * factorial(a - 1);
 };
 
 // Do not edit below this line
