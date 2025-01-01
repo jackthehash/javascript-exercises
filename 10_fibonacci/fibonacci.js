de92cd378661c6445af2a50835557f0cc298432e
jackthehash <ltwltw552@gmail.com>
2025-01-01 22:56:46 +0800

    finished 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,27 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    let n = parseInt(num)
+    let a = 1;
+    let b = 1;
+    if (n === 0) {
+        return 0;
+    }
+    if (n < 0) {
+        return "OOPS";
+    }
+    if (n === 1 || n === 2) {
+        return 1;
+    }
+    for (let i = 0; i < n - 2; i++) {
+        if (i % 2 === 0) {
+            a += b;
+        } else {
+            b += a;
+        }
+    }
+    if (a > b) {
+        return a;
+    } else return b;
+    
 };
 
 // Do not edit below this line
