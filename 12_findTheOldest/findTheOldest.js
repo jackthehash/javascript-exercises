2e5f328d169215b63bbe9f34d5257b430417e37f
jackthehash <ltwltw552@gmail.com>
2025-01-01 23:17:24 +0800

    finished 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,18 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+    arr.sort((a,b) => {
+        let aAge = 0;
+        let bAge = 0;
+        aAge = a.yearOfDeath - a.yearOfBirth;
+        bAge = b.yearOfDeath - b.yearOfBirth;
+        if (!("yearOfDeath" in a)) {
+            aAge = (new Date()).getFullYear() - a.yearOfBirth;
+        }
+        if (!("yearOfDeath" in b)) {
+            bAge = (new Date()).getFullYear() - b.yearOfBirth;
+        }
+        return aAge < bAge ? 1 : -1;
+    })
+    return arr[0];
 };
 
 // Do not edit below this line
